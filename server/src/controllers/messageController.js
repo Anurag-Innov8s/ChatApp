5efc4bd79685e73fa1f29fd3eed1883420e5a9ed
Anurag Kumar <anuraganu202016@gmail.com>
2025-02-28 01:38:45 +0530

    message routes

---
+++ server/src/controllers/messageController.js
@@ -0,0 +1,63 @@
+import User from "../models/userModlel.js";
+import Message from "../models/messageModel.js";
+import cloudinary from "../lib/cloudinary.js";
+export const getUsersForSidebar = async (req, res) => {
+    try {
+        const loggedInUserId = req.user._id;
+        const filterUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+        res.status(200).json(filterUsers);
+
+    } catch (error) {
+        console.error("Error in getting Users", error.message);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+}
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+        const myId = req.user._id;
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: myId }
+            ]
+        })
+        res.status(200).json(messages)
+    } catch (error) {
+
+    }
+}
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upoad(image);
+            imageUrl=uploadResponse.secure_url; 
+        }
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl
+        })
+        await newMessage.save();
+
+// todo: use of socket.io
+        res.status(201).json(newMessage)
+    } catch (error) {
+        console.log("Error in sending message",error.message);
+        res.status(500).json({
+            success:false,
+            message:"Internal Server Error"
+        })
+        
+    }
+}
--- server/src/index.js
+++ server/src/index.js
@@ -2,15 +2,18 @@ import express from "express";
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/authRoutes.js"
+import messageRoutes from "./routes/messageRoutes.js"
 import cookieParser from "cookie-parser"
 
 dotenv.config();
 const PORT = process.env.PORT
 const app = express();
 app.use(express.json()) 
-app.use("/api/auth",authRoutes)
 app.use(express.urlencoded({ extended: true })); // Optional: for form data
+
 app.use(cookieParser());
+app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
 app.listen(PORT,() =>{
     console.log(`Server is runnig on PORT ${PORT}`)
     connectDB();
