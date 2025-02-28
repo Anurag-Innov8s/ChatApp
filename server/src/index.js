4a531813682031997959ee85663efbb846daefe5
Anurag Kumar <anuraganu202016@gmail.com>
2025-03-01 02:34:54 +0530

    starting the frontend

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/authRoutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
 import cookieParser from "cookie-parser"
-
+import cors from "cors";
 dotenv.config();
 const PORT = process.env.PORT
 const app = express();
@@ -12,6 +12,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true })); // Optional: for form data
 
 app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}))
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
 app.listen(PORT,() =>{
