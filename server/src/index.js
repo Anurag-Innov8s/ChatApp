5fda66000359d3bd77c2d69b79137517edf2cd54
Anurag Kumar <anuraganu202016@gmail.com>
2025-03-08 00:25:43 +0530

    deployment changes

--- server/src/index.js
+++ server/src/index.js
@@ -5,10 +5,11 @@ import authRoutes from "./routes/authRoutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
 import cookieParser from "cookie-parser"
 import cors from "cors";
+import path from "path";
 import { app,server } from "./lib/socket.js";
 dotenv.config();
 const PORT = process.env.PORT
-  
+const __dirname = path.resolve();
 app.use(express.json({ limit: "50mb" })); // Increase JSON payload limit
 app.use(express.urlencoded({ extended: true, limit: "50mb" })); // Increase URL-encoded data limit
 
@@ -20,6 +21,15 @@ app.use(cors({
 }))
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
+
 server.listen(PORT,() =>{
     console.log(`Server is runnig on PORT ${PORT}`)
     connectDB();
